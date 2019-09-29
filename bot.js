45b8defdefc9be51388eb36d4d10b48d96728476
DON15NINO <44461351+DON15NINO@users.noreply.github.com>
2019-09-30 01:16:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,3 +38,14 @@ client.user.setGame("الستريم هنا", "https://www.twitch.tv/alpha");
 });
 
 
+client.on('guildMemberAdd', member => {
+  member.guild.fetchInvites().then(guildInvites => {
+    const ei = invites[member.guild.id];
+    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+    const inviter = client.users.get(invite.inviter.id);
+    const xkiller = member.guild.channels.find("name", "vesuvio");
+     xkiller.send(`<@${member.user.id}> تمت الدعوه من <@${inviter.id}>`);
+  });
+});
+
+
